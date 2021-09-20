ff2e5c383089e6c026579182ebca040583678ea5
alexmlamb <lambalex@iro.umontreal.ca>
2021-09-19 18:58:53 -0700

    Add quantize.py

--- dall_e/main.py
+++ dall_e/main.py
@@ -13,18 +13,25 @@ from encoder import Encoder
 from decoder import Decoder
 from quantize import Quantize
 
+from laplace_loss import lap_transform, lap_inv_transform, lap_loss
+
 enc = Encoder(vocab_size=256).cuda()
 dec = Decoder(vocab_size=256).cuda()
 
+#mlp = nn.Sequential(nn.Linear(4*4*256, 4*4*256), nn.LeakyReLU(), nn.Linear(4*4*256,4*4*256)).cuda()
+
 bs = 256
 L = 1024
-n_factors = 4
+n_factors = 16
 
 assert bs*n_factors >= L
 
 quant = Quantize(4*4*256, L, n_factors).cuda() # 1 factor
 
-opt = torch.optim.Adam(list(enc.parameters()) + list(dec.parameters()) + list(quant.parameters()))
+params = list(enc.parameters()) + list(dec.parameters()) + list(quant.parameters())
+opt = torch.optim.AdamW(params, weight_decay=1e-4)
+
+scheduler = torch.optim.lr_scheduler.MultiStepLR(opt, [50,80])
 
 hw = 32
 
@@ -40,7 +47,7 @@ train_loader = torch.utils.data.DataLoader(datasets.CIFAR10('data',
                                            shuffle=True)
 
 
-for epoch in range(0,50):
+for epoch in range(0,100):
 
     loss_lst = []
     k = 0
@@ -57,16 +64,22 @@ for epoch in range(0,50):
 
         #can quantize here
 
-        if epoch >= 3:
+        #h = mlp(h)
+
+        if epoch >= 10:
             h, q_loss, ind = quant(h)
         else:
             q_loss = 0.0
 
         h = h.reshape((bs, e_width, e_width, e_dim)).permute(0,3,1,2)
 
-        xr = dec(h)[:,0:3]
+        xr = dec(h)
 
+        xr = xr[:,0:3]
         loss = F.mse_loss(x, xr)
+      
+        #loss = lap_loss(lap_transform(x), xr[:,0:3], xr[:,3:6])
+        #xr = lap_inv_transform(F.sigmoid(xr[:,0:3]))
 
         all_loss = loss + q_loss
 
@@ -85,6 +98,6 @@ for epoch in range(0,50):
             save_image(xr[:64], 'rec.png')
 
 
-
+    scheduler.step()
 
 
